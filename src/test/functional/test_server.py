a242d7159032ce707e60d14b376085641b2588d5
Olivier Collier <oliviercollier8@gmail.com>
2021-05-11 19:36:06 +0200

    :sparkles: Adding functional test

--- src/test/functional/test_server.py
+++ src/test/functional/test_server.py
@@ -1,44 +1,63 @@
-import requests
-
 from src.config.config import config
+from src.application.server import app
 
-api = config["api"]
-host = api["host"]
-port = api["port"]
+from fastapi.testclient import TestClient
 
-def test_example1():
-    endpoint = "example"    
-    url = f"http://{host}:{port}/{endpoint}"
-    headers = {"Content-type": "application/json"}
+PORT = config["api"]["port"]
+HOST = config["api"]["host"]
 
-    data = {"question": 2}
+client = TestClient(app)
 
-    response = requests.get(url=url, json=data, headers=headers)
+
+def test_predict():
+    endpoint = "predict"
+    headers = {"Content-Type": 'application/json'}
+    data = {"DATE":"2010-02-24",\
+            "AGE": 0,\
+            "JOB_TYPE": "Admin",\
+            "STATUS": "Marié",\
+            "EDUCATION": "Primaire",\
+            "HAS_DEFAULT": "No",\
+            "BALANCE": 0,\
+            "HAS_HOUSING_LOAN": "No",\
+            "HAS_PERSO_LOAN": "Yes",\
+            "CONTACT": "Portable",\
+            "DURATION_CONTACT": 99,\
+            "NB_CONTACT": 2,\
+            "NB_DAY_LAST_CONTACT": 86,\
+            "NB_CONTACT_LAST_CAMPAIGN": 5,\
+            "RESULT_LAST_CAMPAIGN": "Echec"}
+    response = client.post(f"/{endpoint}", json=data, headers=headers)
     results = response.json()
-    
-    assert results["answer"] == data["question"]*2
-    
-def test_example2():
-    endpoint = "example"    
-    url = f"http://{host}:{port}/{endpoint}"
-    headers = {"Content-type": "application/json"}
-
-    data = {"question": "a"}
-
-    response = requests.get(url=url, json=data, headers=headers)
-    results = response.json()
-    
-    assert results["answer"] == 0
+    assert results['predictions'] == "A souscrit"
 
 
-def test_example3():
-    endpoint = "example"    
-    url = f"http://{host}:{port}/{endpoint}"
-    headers = {"Content-type": "application/json"}
+#def test_example1():
+#    endpoint = "example"
+#    url = f"http://{host}:{port}/{endpoint}"
+#    headers = {"Content-type": "application/json"}
+#
+#    data = {"question": 2}
+#
+#    response = requests.get(url=url, json=data, headers=headers)
+#    results = response.json()
+#    assert results["answer"] == data["question"]*2
 
-    data = {"wrong_field": "a"}
+#def test_example2():
+#    endpoint = "example"
+#    url = f"http://{host}:{port}/{endpoint}"
+#    headers = {"Content-type": "application/json"}
+#    data = {"question": "a"}
+#    response = requests.get(url=url, json=data, headers=headers)
+#    results = response.json()
+#    assert results["answer"] == 0
 
-    response = requests.get(url=url, json=data, headers=headers)
-    results = response.json()
-    
-    assert results["answer"] == 0
+
+#def test_example3():
+#    endpoint = "example"
+#    url = f"http://{host}:{port}/{endpoint}"
+#    headers = {"Content-type": "application/json"}
+#    data = {"wrong_field": "a"}
+#    response = requests.get(url=url, json=data, headers=headers)
+#    results = response.json()
+#    assert results["answer"] == 0
