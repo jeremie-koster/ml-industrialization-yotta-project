139ef621aec8e138c4930ee632d2c42e506108a9
Olivier Collier <oliviercollier8@gmail.com>
2021-05-14 21:36:46 +0200

    :sparkles: Adding sentry

--- src/application/server.py
+++ src/application/server.py
@@ -1,13 +1,22 @@
-import datetime
-
-import pandas as pd
-import uvicorn
-from fastapi import FastAPI
-from pydantic import BaseModel
-
 from src.application.predict import main as prediction_pipeline
+from src.config.config import config
+
+import datetime
+from fastapi import FastAPI
+import pandas as pd
+from pydantic import BaseModel
+import sentry_sdk
+import uvicorn
+
+sentry_sdk.init(
+    "https://10cea8131a31434c9e96fe6e324dcf16@o663085.ingest.sentry.io/5765736",
+    traces_sample_rate=1.0,
+)
 
 app = FastAPI()
+
+PORT = config["api"]["port"]
+HOST = config["api"]["host"]
 
 
 class Prediction(BaseModel):
@@ -27,14 +36,19 @@ def example(input: dict):
     return response
 
 
-@app.get("/predict", response_model=Prediction)
+@app.post("/predict", response_model=Prediction)
 def prediction(input: dict):
 
-    input_df = pd.DataFrame(input, index=[0])
-    csv_path = "~/sample.csv"
-    input_df.to_csv(csv_path, index=False)
+    try:
+        input_df = pd.DataFrame(input, index=[0])
+        csv_path = "~/sample.csv"
+        input_df.to_csv(csv_path, index=False)
+        result_subscription = prediction_pipeline(csv_path, run_type="api")
+    except (KeyError, TypeError, ValueError) as error:
+        DEFAULT_RESULT = "Error"
+        result_subscription = DEFAULT_RESULT
+        sentry_sdk.capture_exception(error)
 
-    result_subscription = prediction_pipeline(csv_path, run_type="api")
     return {"predictions": result_subscription}
 
 
@@ -45,10 +59,5 @@ def index():
 
 
 if __name__ == "__main__":
-    from src.config.config import config
-
-    PORT = config["api"]["port"]
-    HOST = config["api"]["host"]
-
     print("starting API at", datetime.datetime.now())
     uvicorn.run("src.application.server:app", host=HOST, port=PORT)
